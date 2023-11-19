5dec0179addf26b47a3a725ab0ba2a0a17cd5302
Oleg Shevtsov <olegshevtsov1@gmail.com>
2023-11-19 18:00:29 +0200

    terraform-dynamic: ecs

---
+++ ecs/variables.tf
@@ -0,0 +1,15 @@
+# environment variables
+variable "region" {}
+variable "project_name" {}
+variable "environment" {}
+
+# ecs variables
+variable "ecs_task_execution_role_arn" {}
+variable "architecture" {}
+variable "container_image" {}
+variable "env_file_bucket_name" {}
+variable "env_file_name" {}
+variable "private_app_subnet_az1_id" {}
+variable "private_app_subnet_az2_id" {}
+variable "app_server_security_group_id" {}
+variable "alb_target_group_arn" {}
