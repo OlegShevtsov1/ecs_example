fc33fe4b1286944ae691f117a86f7c40e587e537
Oleg Shevtsov <olegshevtsov1@gmail.com>
2023-12-07 09:15:10 +0200

    terraform-dynamic: fix public_subnet_2_rt to public_subnet_az2_rt

--- vpc/main.tf
+++ vpc/main.tf
@@ -66,7 +66,7 @@ resource "aws_route_table_association" "public_subnet_az1_rt_association" {
 }
 
 # associate public subnet az2 to "public route table"
-resource "aws_route_table_association" "public_subnet_2_rt_association" {
+resource "aws_route_table_association" "public_subnet_az2_rt_association" {
   subnet_id      = aws_subnet.public_subnet_az2.id
   route_table_id = aws_route_table.public_route_table.id
 }
